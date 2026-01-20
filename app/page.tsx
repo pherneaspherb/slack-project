704db510cf9d1ad26843b0678a2e8fcc4d7ed881
[pherneaspherb] <[johnpherloyd.rangas@wvsu.edu.ph]>
2026-01-20 22:41:33 +0800

    modified app/globals.css, app/page.tsx, package-lock.json, package.json, postcss.config.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="text-rose-500 font-bold text-2xl">
-      Hello World
-    </div>
+    <Button size="lg">Click Me</Button>
   );
 };
